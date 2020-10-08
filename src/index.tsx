c7881914dc0cdd03ff1b0431c38f1828611ba487
danmaq <info@danmaq.com>
2020-10-08 15:05:27 +0900

    implemented the context

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Contexts from './hooks/contexts';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Contexts>
+      <App />
+    </Contexts>
   </React.StrictMode>,
   document.getElementById('root')
 );
