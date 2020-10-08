befb6c259ed86e8b081f01e2b7589bc43048eb69
danmaq <info@danmaq.com>
2020-10-08 22:38:17 +0900

    implemented the actions

--- src/hooks/contexts/counter/actions.ts
+++ src/hooks/contexts/counter/actions.ts
@@ -2,18 +2,23 @@ import React from 'react';
 import { createContainer } from 'unstated-next';
 import reducer, { add, set } from './reducer';
 
+export interface Actions {
+  readonly add: () => void;
+  readonly set: (value: number) => void;
+}
+
 const useActions = () => {
   const [, dispatch] = reducer.useContainer();
-  return {
+  return Object.freeze<Actions>({
     add: React.useCallback(() => dispatch({ type: add }), [dispatch]),
     set: React.useCallback(
       (value: number) => dispatch({ type: set, payload: value }),
       [dispatch]
     ),
-  } as const;
+  });
 };
 
 const Container = createContainer(useActions);
-Container.Provider.displayName = '[ACTIONS] Counter';
+Container.Provider.displayName = 'CounterAction';
 
 export default Container;
