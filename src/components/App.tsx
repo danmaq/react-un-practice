a862a019367fa314c46bb4a126d842cb4226ac88
danmaq <info@danmaq.com>
2020-10-08 16:07:13 +0900

    fixed fails the tests

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import Contexts from '../hooks/contexts';
 import logo from './logo.svg';
 import Counter from './Counter';
 import Fetch from './Fetch';
 import './App.css';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello, world!</p>
-      <Counter />
-      <Fetch />
-    </header>
-  </div>
+  <Contexts>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello, world!</p>
+        <Counter />
+        <Fetch />
+      </header>
+    </div>
+  </Contexts>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import Contexts from './hooks/contexts';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Contexts>
-      <App />
-    </Contexts>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
