5aa96d1296acd65a273662cbe0f27ee55e11ee17
danmaq <info@danmaq.com>
2020-10-08 16:56:03 +0900

    moved the combine component

--- src/hooks/contexts/index.tsx
+++ src/hooks/contexts/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import Combine from './combine';
-import type { Register } from './base/createCombineProps';
+import Combine from './base/combine';
+import createCombineProps, { Register } from './base/createCombineProps';
 import counterContext from './counter';
 import fetchContext from './fetch';
 
 const contexts: readonly Register[] = [counterContext, fetchContext];
 
-const Contexts: React.FC = ({ children }) => (
-  <Combine
-    actions={contexts.flatMap(({ actions }) => actions)}
-    reducers={contexts.map(({ reducer }) => reducer)}
-  >
-    {children}
-  </Combine>
-);
-Contexts.displayName = 'Contexts';
+const Component: React.FC = ({ children }) => {
+  const { actions, reducers } = createCombineProps(contexts);
+  return (
+    <Combine actions={actions} reducers={reducers}>
+      {children}
+    </Combine>
+  );
+};
+Component.displayName = 'Contexts';
 
-export default Contexts;
+export default Component;
