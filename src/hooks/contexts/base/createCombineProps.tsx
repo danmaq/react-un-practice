2da304ab984b11be1745e5c37ce923e5c2821757
danmaq <info@danmaq.com>
2020-10-08 17:07:37 +0900

    improved the types

--- src/hooks/contexts/base/createCombineProps.tsx
+++ src/hooks/contexts/base/createCombineProps.tsx
@@ -3,12 +3,16 @@ import type { ContainerProviderProps } from 'unstated-next';
 import type { Props } from './combine';
 
 export interface Register {
-  readonly actions: readonly React.ComponentType<ContainerProviderProps>[];
-  readonly reducer: React.ComponentType<ContainerProviderProps>;
+  readonly actions?: readonly React.ComponentType<ContainerProviderProps>[];
+  readonly reducer?: React.ComponentType<ContainerProviderProps>;
 }
 
 export default (contexts: readonly Register[]) =>
   Object.freeze<Props>({
-    actions: contexts.flatMap(({ actions }) => actions),
-    reducers: contexts.map(({ reducer }) => reducer),
+    actions: contexts
+      .flatMap(({ actions }) => actions)
+      .filter((v): v is React.ComponentType<ContainerProviderProps> => !!v),
+    reducers: contexts
+      .map(({ reducer }) => reducer)
+      .filter((v): v is React.ComponentType<ContainerProviderProps> => !!v),
   });
