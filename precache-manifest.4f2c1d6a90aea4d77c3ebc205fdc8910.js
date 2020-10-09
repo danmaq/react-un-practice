28252a99951b65ba5c5a58537b572b4ec5372baf
danmaq <danmaq@users.noreply.github.com>
2020-10-09 07:33:28 +0000

    deploy: 8e33c3730a9f2012fd7f307477b4eb0265f5b8d5

---
+++ precache-manifest.4f2c1d6a90aea4d77c3ebc205fdc8910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bde6435c7176a81a764b2751ee1bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bb5d7223543fe783f75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9337e85016f18f365a0a",
+    "url": "/static/js/2.ae20fd57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae20fd57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb5d7223543fe783f75",
+    "url": "/static/js/main.6d2db121.chunk.js"
+  },
+  {
+    "revision": "0f3a8a1a073654ca7d59",
+    "url": "/static/js/runtime-main.5b65ac6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
