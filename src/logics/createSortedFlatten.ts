aa51befbc3df953f6939fd528fdd7da0a56fc129
danmaq <info@danmaq.com>
2020-10-14 12:24:36 +0900

    update flatten function

--- src/logics/createSortedFlatten.ts
+++ src/logics/createSortedFlatten.ts
@@ -1,5 +1,14 @@
 /**
- * 指定した引数をルールに、ラスタライズ関数を生成します。
+ * レイヤー構造として要求する型定義。
+ * @template R レイヤーの優先度として、使用する型。
+ * @template T レイヤー本体の型。
+ */
+export type Layer<R extends PropertyKey, T> = Readonly<
+  Partial<Record<R, T | readonly T[]>>
+>;
+
+/**
+ * 指定した引数をルールに、レイヤー結合関数を生成します。
  * @template R レイヤーの優先度として、使用する型。
  *
  * キーとして列挙可能な型のみ使用可能です。
@@ -7,11 +16,14 @@
  *
  * インデックスが先頭の要素ほど、先頭に配置します。
  */
-export default <R extends PropertyKey>(order: readonly R[]) => <T>(
-  /** レイヤー別に分類した、値もしくは値の一覧。 */
-  ...layers: Record<R, T | readonly T[]>[]
-) =>
-  order.reduce<T[]>((acc, key) => {
-    const list = layers.flatMap((x): T | readonly T[] => x[key]);
-    return [...acc, ...list];
-  }, []);
+export default <R extends PropertyKey>(order: readonly R[]) =>
+  /**
+   * レイヤーを結合して、単一の配列を生成します。
+   * @template T レイヤーの型。
+   * @param layers レイヤー別に分類した、値もしくは値の一覧。
+   */
+  <T>(...layers: Layer<R, T>[]) =>
+    order.reduce<T[]>((acc, key) => {
+      const list = layers.flatMap((x): T | readonly T[] => x[key] ?? []);
+      return [...acc, ...list];
+    }, []);
