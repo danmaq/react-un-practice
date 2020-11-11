176ed28dae237dc5782ad3c79113d76b0728e093
danmaq <info@danmaq.com>
2020-11-11 15:13:28 +0900

    added the reducer for form

---
+++ src/hooks/contexts/form/reducer/text.ts
@@ -0,0 +1,33 @@
+import type React from 'react';
+import type { Action, ReducerSource } from '../../base/createReducer';
+
+/** 文字列の上書きを示す、識別定数。 */
+export const set = Symbol('set');
+
+/** 文字列の状態における、型定義。 */
+export interface TextState {
+  /** 文字列。 */
+  readonly text: string;
+}
+
+/** 文字列用のアクション パラメーター。 */
+export type TextAction = Action<typeof set, string>;
+
+/**
+ * リデューサー関数。
+ * @param state 直近の状態。
+ * @param action ディスパッチ アクションのパラメーター。
+ */
+const reducer: React.Reducer<TextState, TextAction> = (state, action) => {
+  switch (action.type) {
+    case set:
+      return { ...state, text: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default Object.freeze<ReducerSource<typeof reducer>>({
+  initial: { text: '' },
+  reducer,
+});
